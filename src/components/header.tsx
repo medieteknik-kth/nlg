8ca38529b9ef7c25954504b1694c8f4a8b2c1e19
martinryberglaude <martin.ryberg.laude@gmail.com>
2020-12-08 17:45:42 +0100

    Add proper header w/ navbar

---
+++ src/components/header.tsx
@@ -0,0 +1,42 @@
+import { Link } from 'gatsby'
+import React from 'react'
+import styles from './header.module.scss'
+import logo from 'src/images/nlg-icon.png'
+
+interface Props {
+  // Page number, first page is 0
+  page: number
+}
+export default function Header(props: Props) {
+
+  // Adds active styles if page number matches
+  function getActiveClass(page: number): string {
+    return page === props.page ? "active" : ""
+  }
+
+  return (
+    <header className={styles.container}>
+        <Link className={styles.titleContainer} to="">
+          <img className={styles.logo} src={logo}/>
+          <h1 className={styles.title}>Näringslivsgruppen</h1>
+        </Link>
+      <nav>
+        <ul>
+          {/* TODO: Add proper links */}
+          <li>
+            <Link className={styles.navItem + " " + styles[getActiveClass(0)]} to="">Hem</Link>
+          </li>
+          <li>
+            <Link className={styles.navItem + " " + getActiveClass(1)} to="">Timeline</Link>
+          </li>
+          <li>
+            <Link className={styles.navItem + " " + getActiveClass(2)} to="">Kontakt</Link>
+          </li>
+          <li>
+            <Link className={styles.navItem + " " + getActiveClass(3)} to="">Om oss</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import SEO from "src/components/seo"
+import Header from "src/components/header"
+
+export default function Index() {
+  return (
+    <div>
+      <SEO title="Hem"/>
+      <Header page={0}/>
+      
+    </div>
+  )
+}
