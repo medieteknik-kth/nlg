18387d945848dc53f21e276217b0463f9db9a870
Kristina Andersson <kristinaandersson@Kristinas-MacBook-Pro-2.local>
2020-12-14 10:17:48 +0100

    create timeline post template

---
+++ src/templates/timeline.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const timeline: React.FC = () => {
+  return (
+    <Layout>
+      <SEO title="Timeline Post" />
+      This is where the timeline post will be shown
+    </Layout>
+  )
+}
+
+export default timeline
