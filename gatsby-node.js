cc66e18b37c3c7dfd9c2f57791525fa4a653238f
Kristina Andersson <kristinaandersson@Kristinas-MacBook-Pro-2.local>
2020-12-14 10:19:34 +0100

    generate timeline post page for each .md file

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,9 +4,37 @@ module.exports.onCreateNode = ({ node, actions }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === "MarkdownRemark") {
-    // reduces path to just the filename
+    // reduce path to just the filename
     const slug = path.basename(node.fileAbsolutePath, ".md")
 
     createNodeField({ node, name: "slug", value: slug })
   }
+}
+
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const timelineTemplate = path.resolve("./src/templates/timeline.tsx")
+  const response = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  response.data.allMarkdownRemark.edges.forEach(edge => {
+    createPage({
+      component: timelineTemplate,
+      path: `/timeline/${edge.node.fields.slug}`,
+      context: {
+        slug: edge.node.fields.slug,
+      },
+    })
+  })
 }
