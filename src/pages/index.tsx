fdf5af597fba6e6012b026dc889d24c9bb90b1e4
Martin R-L <Martin Ryberg Laude>
2020-12-20 19:31:37 +0100

    Add basic home hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,27 @@
 import React from "react"
 import SEO from "src/components/seo"
 import Header from "src/components/header"
+import styles from './index.module.scss'
+import { Link } from "gatsby"
+import { FaFacebook, FaInstagram} from 'react-icons/fa'
 
 export default function Index() {
   return (
-    <div>
+    <div className={styles.containerMain}>
       <SEO title="Hem" />
       <Header page={0} />
+      <div className={styles.containerHero}>
+        <h1>Näringslivsgruppen</h1>
+        <p>Lorem ipsum dolor sit amet</p>
+        <div className={styles.containerLinks}>
+          <Link className={"btn " + styles.btn} to="/contact">Kontakta oss</Link>
+          <Link className={"btn " + styles.btnAlt} to="/">Läs mer</Link>
+        </div>
+      </div>
+      <div className={styles.containerMedia}>
+        <Link to="https://www.facebook.com/NLGmedieteknik"><FaFacebook className={styles.icon}/></Link>
+        <Link to="http://www.instagram.com/nlg_medieteknik"><FaInstagram className={styles.icon}/></Link>
+      </div>
     </div>
   )
 }
