a6b9bc916cd86f395e036afbe27e85f703042d05
Martin R-L <Martin Ryberg Laude>
2021-02-23 16:55:51 +0100

    Add proper footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,28 +1,47 @@
 import React from 'react'
-import { useStaticQuery, graphql } from "gatsby"
 import styles from './footer.module.scss'
+import logo from "src/images/nlg-icon.png"
+import { Link } from 'gatsby'
 
 export default function Footer() {
-  const data = useStaticQuery(graphql`
-    query HomePageQuery {
-      site {
-        siteMetadata {
-          title
-          author
-        }
-      }
-    }
-  `)
-
+ 
   return (
-    <footer className={styles.containerMain}>
-      <p>
-      © {new Date().getFullYear()},{" "}
-          {data.site.siteMetadata.author.map(
-            (author: string, i: number, arr: []) =>
-              `${author} ${i != arr.length - 1 ? "och " : ""}`
-          )}
-      </p>
+    <footer>
+      <div className={styles.containerMain}>
+        <div className={styles.column + " " + styles.columnLogo}>
+          <img className={styles.logo} src={logo}/>
+        </div>
+        <div className={styles.column}>
+          <div className={styles.contact}>
+            <h3>Kontakt</h3>
+            <a href="mailto: naringsliv@medieteknik.com">naringsliv@medieteknik.com</a>
+          </div>
+          <div>
+            <p>Fack vid THS</p>
+            <p>Drottning Kristinas Väg 15</p>
+            <p>100 44 Stockholm</p>
+          </div>
+        </div>
+        <div className={styles.column}>
+          <div className={styles.split}>
+            <div className={styles.links}>
+              <h3>Länkar</h3>
+              <Link to="/">Hem</Link>
+              <Link to="/">Tidslinje</Link>
+              <Link to="/contact">Samarbete</Link>
+              <Link to="/">Om oss</Link>
+            </div>
+            <div className={styles.links}>
+              <h3>Socialt</h3>
+              <a href="https://www.facebook.com/NLGmedieteknik">Facebook</a>
+              <a href="http://www.instagram.com/nlg_medieteknik">Instagram</a>
+            </div>
+          </div>
+          <p className={styles.copyright}>
+          © {new Date().getFullYear()  + " Näringslivsgruppen Medieteknik | KTH"}
+          </p>
+        </div>
+      </div>
     </footer>
   )
 }
