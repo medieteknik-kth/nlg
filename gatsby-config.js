2d9be91e47d2cb1a3d1d26fe969622fae9735c54
Kristina Andersson <kristina-andersson@outlook.com>
2021-03-08 12:18:09 +0100

    configure gatsby-source-contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,13 @@ module.exports = {
     author: [`Kristina Andersson`, `Martin Ryberg Laude`],
   },
   plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     "gatsby-plugin-root-import",
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
